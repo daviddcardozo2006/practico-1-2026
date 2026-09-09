dfe78d7466484120296c728c0211b236f57463ac
David <daviddcardozo2006@gmail.com>
2026-09-09 00:48:01 -0300

    parte 2 finalizada, 100 %

--- src/parte 2/cuenta.ts
+++ src/parte 2/cuenta.ts
@@ -12,22 +12,39 @@ export class Cuenta {
     constructor(numero: number, titular: string, saldoInicial: number = 0) {
         // TODO: asignar numero y titular. Validar que saldoInicial no sea
         // negativo (si lo es, lanzar un error) y asignarlo a saldo.
-        throw new Error("Implementar");
+        if (saldoInicial<0){
+            throw new Error ("Ingresá un saldo positivo.")
+        }
+        this.numero=numero;
+        this.titular=titular;
+        this.saldo = saldoInicial;
+        
     }
 
     depositar(monto: number): void {
         // TODO: no se pueden depositar valores negativos -> throw new Error(...)
-        throw new Error("Implementar");
+        if (monto<0){
+            throw new Error ("Ingresá un monto positivo.");
+        }
+        this.saldo += monto;
+        
+        
     }
 
     retirar(monto: number): void {
         // TODO: no se pueden retirar valores negativos, ni retirar más
         // dinero del disponible -> throw new Error(...)
-        throw new Error("Implementar");
+        if (monto<0){
+            throw new Error ("Ingresá un monto a retirar positivo.");
+        }else if (monto > this.saldo){
+            throw new Error ("Saldo insuficiente");
+        }
+        this.saldo -= monto;
+        
+        
     }
 
     consultarSaldo(): number {
-        // TODO
-        throw new Error("Implementar");
+        return this.saldo;
     }
 }
